b165302286eb2871d34786ee04ad466f459f9fb1
Philipp Hörist <philipp@hoerist.com>
2024-04-29 22:14:38 +0200

    Simplify init of _EMOJI_UNICODE cache

--- emoji/unicode_codes/__init__.py
+++ emoji/unicode_codes/__init__.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Dict
 from emoji.unicode_codes.data_dict import *
 from emoji.unicode_codes.data_dict import LanguagesT
 
@@ -8,7 +8,7 @@ __all__ = [
 ]
 
 
-_EMOJI_UNICODE: Dict[LanguagesT, Any] = {lang: None for lang in LANGUAGES}  # Cache for the language dicts
+_EMOJI_UNICODE: Dict[LanguagesT, Dict[str, str]] = {}  # Cache for the language dicts
 
 _ALIASES_UNICODE: Dict[str, str] = {}  # Cache for the aliases dict
 
@@ -17,7 +17,7 @@ def get_emoji_unicode_dict(lang: LanguagesT) -> Dict[str, str]:
     """Generate dict containing all fully-qualified and component emoji name for a language
     The dict is only generated once per language and then cached in _EMOJI_UNICODE[lang]"""
 
-    if _EMOJI_UNICODE[lang] is None:
+    if lang not in _EMOJI_UNICODE:
         _EMOJI_UNICODE[lang] = {
             data[lang]: emj for emj, data in EMOJI_DATA.items()
             if lang in data and data['status'] <= STATUS['fully_qualified']
